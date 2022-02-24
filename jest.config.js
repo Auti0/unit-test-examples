f1e675f9c9e5b1d3aa647f9e5cb360d0208d027d
Andrew Davis <andrew.davis@continu.com>
2022-02-23 19:54:57 -0600

    iterating on github actions is annoying pt 6

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   setupFiles: ['<rootDir>/tests/js/setupTests.js'],
-  testMatch: ['**/tests/js/**/*.test.js'],
-  collectCoverage: true,
-  coverageReporters: ['lcov'],
-  coverageProvider: 'babel',
-  collectCoverageFrom: ['src/js/*.js'],
-  coverageDirectory: '.coverage'
+  testMatch: ['**/tests/js/**/*.test.js']
 };
